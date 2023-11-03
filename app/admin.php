aaeaf786726e13d93f911decd1454faf025c6427
KnurBorzy <dominikmartynow@gmail.com>
2023-11-03 09:24:36 +0100

    gitignore change

--- app/admin.php
+++ app/admin.php
@@ -10,7 +10,7 @@
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Admin</title>
-    <link rel="stylesheet" href="/style/admin-style.css">
+    <link rel="stylesheet" href="style/admin-style.css">
 </head>
 <body>
     <header>
